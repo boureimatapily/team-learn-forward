e23f7d782237ccfa73bfdd7947e3a22faf6984fd
unknown <boureimatapily1@gmail.com>
2020-06-29 18:50:11 +0000

    MOD add content

--- src/Components/Home/MainContent/Welcome.js
+++ src/Components/Home/MainContent/Welcome.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginLeft:10,
+  },
+  
+}));
 
 export default function Welcome() {
+    const classes = useStyles();
     return (
-        <div>
+        <div className={classes.root}>
             <h1> Learn,</h1>
             <h1>Validate,</h1>
             <h1>Collaborate.</h1>
