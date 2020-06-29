dba261f550ff4a24a0e05e57cb3e77f302e52d74
unknown <boureimatapily1@gmail.com>
2020-06-29 09:56:25 +0000

    MOD add class base Component and Route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Home from './Pages/Home';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
+import Courses from './Pages/Courses';
+import { Switch, Route, } from 'react-router-dom';
+import NavBar from './Components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  render() {
+    return (
+     <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/courses" component={Courses} />
+        </Switch>
+     </React.Fragment>
+        
+    )
+  }
 }
 
-export default App;
