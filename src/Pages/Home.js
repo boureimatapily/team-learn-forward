69a5fcf540b40be2f2df71dc6675720d7b46abdc
unknown <boureimatapily1@gmail.com>
2020-06-29 15:33:30 +0000

    MOD render Maincontent.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import MainContent from '../Components/Home/MainContent'
+
+
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                
+                <MainContent />
             </div>
         )
     }
