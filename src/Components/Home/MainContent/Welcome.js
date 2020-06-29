1e95e481b7f9cd51309e03b2d0bfd2daf5d10d03
unknown <boureimatapily1@gmail.com>
2020-06-29 15:33:06 +0000

    MOD add content

---
+++ src/Components/Home/MainContent/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Welcome() {
+    return (
+        <div>
+            <h1> Learn,</h1>
+            <h1>Validate,</h1>
+            <h1>Collaborate.</h1>
+            <h3>Join over 1000 students</h3>
+        </div>
+    )
+}
