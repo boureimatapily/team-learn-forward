89e1d239119f4e5903dd4c6ea242a68e1f2febeb
unknown <boureimatapily1@gmail.com>
2020-07-01 22:31:48 +0000

    MOD create content

--- src/Components/Home/MainContent/MainCard.js
+++ src/Components/Home/MainContent/MainCard.js
@@ -1,34 +1,83 @@
 import React  from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-// import CardActions from "@material-ui/core/CardActions";
-// import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-// import Button from "@material-ui/core/Button";
-// import Typography from "@material-ui/core/Typography";
-import mimi from "../../../Images/mimi.PNG";
+
+
+import Paper from "@material-ui/core/Paper";
+
+import Section1Card from "./Section1Card";
+// import images
+
+import cardimg from "../../../Images/cardimg.png";
+import learn from "../../../Images/learn.png";
+import money from "../../../Images/money.png";
+
+
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 400,
+ 
+  section1: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(5),
+      height: theme.spacing(5),
+    },
+    marginTop: 20,
+    marginLeft: "10%",
   },
-  media: {
-    height: 400,
-    width: 400,
+   PapperSection1: {
+    width: 250,
+    height: 200,
   },
+  
 }));
 
 export default function MainCard() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia className={classes.media} image={mimi} />
-        </CardActionArea>
-      </Card>
+    <div>
+      <div className={classes.section1}>
+      <Paper className={classes.PapperSection1}>
+              <Section1Card
+                image={learn}
+                infos=" Enroll Now"
+                link="/courses"
+                infoText="Interactive Courses"
+                infoTextContent=" acquire new knowledge, skills, work on real project"
+              />
+            </Paper>
+           
+            <Paper className={classes.PapperSection1}>
+              <Section1Card
+                image={cardimg}
+                infos=" Enroll Now"
+                link="/courses"
+                infoText="Mentorship"
+                infoTextContent="Become a mentors, support our Student and Teachers"
+                
+              />
+            </Paper>
+            <Paper className={classes.PapperSection1}>
+              <Section1Card
+                image={money}
+                infos="Donate"
+                link="/donation"
+                infoText="Make a Donation"
+                infoTextContent="Help us, all the fund goes for Education purpose, "
+              />
+            </Paper>
+            <Paper className={classes.PapperSection1}>
+              <Section1Card
+                image={money}
+                infos="Invest"
+                link="/investment"
+                infoText="Investment Opportunities"
+                infoTextContent="Invest in Education, get realtime update and report"
+              />
+            </Paper>
+      </div>
     </div>
   );
 }
