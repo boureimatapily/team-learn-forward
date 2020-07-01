c8aca19bda549606b5b57e4f60e3cf4bbccd7921
unknown <boureimatapily1@gmail.com>
2020-07-01 22:32:11 +0000

    MOD update style

--- src/Components/Home/MainContent/Section1Card.js
+++ src/Components/Home/MainContent/Section1Card.js
@@ -1,57 +1,72 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
 
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
 
-
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        maxWidth: 250,
-        maxHeight:250,
-      },
-      media: {
-        height: 100,
-        width: 200,
-      },
-      cardButton:{
-          
-          marginTop:10,
-          height:60,
-      }
-  
+  root: {
+    maxWidth: 250,
+    maxHeight: 300,
+    backgroundColor:"#a74fff",
+   
+  },
+  media: {
+    height: 80,
+    width: 80,
+    marginLeft: "10%",
+    marginRight: "10%",
+    marginTop:20,
+    borderRadius:30,
+  },
+  cardButton: {
+    height: 60,
+    width: 150,
+    backgroundColor:"#810aff"
+  },
+  cardinfos:{
+    paddingLeft:20,
+    color:"#0a0a0a",
+    fontWeight:"bold"
+    
+  },
 }));
 
-export default function Section1Card({image, infos, link}) {
+export default function Section1Card({
+  image,
+  infos,
+  link,
+  infoText,
+  infoTextContent,
+}) {
   const classes = useStyles();
- 
+
   return (
     <div className={classes.root}>
-         <Link to={link}>
-            <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={image}
-        />
-        <CardContent className={classes.cardinfos}>
-         
-          
-         
-            {" "}
-            <Button variant="contained" color="primary" className={classes.cardButton}>
-                    <h3>{infos}</h3>
-          </Button>
-         
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    </Link>
+      <Link to={link}>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia className={classes.media} image={image} />
+            <CardContent className={classes.cardinfos}>
+              {" "}
+              <h3> {infoText} </h3>
+              <h4> {infoTextContent} </h4>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.cardButton}
+              >
+                <h3>{infos}</h3>
+              </Button>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Link>
     </div>
   );
 }
