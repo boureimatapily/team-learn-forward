c4890c14a5e3d215254983bc5fbfb68480259f56
unknown <boureimatapily1@gmail.com>
2020-07-02 21:28:08 +0000

    MOD store

---
+++ src/redux/Store.js
@@ -0,0 +1,23 @@
+import firebaseApp from "firebase/app";
+import firebase from "../Config/fbconfig";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
+import { verifyAuth } from "./Actions/authActions";
+import rootReducer from "./Reducers/rootReducer";
+import { firestoreReducer, reduxFirestore } from "redux-firestore";
+
+
+
+let middleware = [applyMiddleware(reduxThunk)];
+
+export default function store(persistedState) {
+    const store = createStore(
+      rootReducer,
+      persistedState,
+      compose(...middleware, reduxFirestore(firebaseApp, firebase))
+    );
+    store.dispatch(verifyAuth());
+    return store;
+  }
+
+  
