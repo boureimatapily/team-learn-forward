1e4c1ae43dada34a9ffcb5c8054cb1bc321b4999
unknown <boureimatapily1@gmail.com>
2020-07-19 23:06:21 +0000

    MOD change app name

--- src/Pages/Courses.js
+++ src/Pages/Courses.js
@@ -1,13 +1,21 @@
+import React from 'react'
 import { withStyles } from "@material-ui/styles";
 
-const styles = {}
+const styles = {
+    root: {
+        flexGrow: 1,
+      },
+      
+}
 
 class Courses extends React.Component{
  
   render(){
      const { classes } = this.props;
     return(
-        <h1>ALL courses</h1>
+        <div style={classes.root}>
+            <h1>ALL courses</h1>
+        </div>
     )
   }
 }
